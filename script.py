7f7d32f8c380bc7269944bda9261bc0736ee4444
Gautam Sachdeva <56038800+gautam232019@users.noreply.github.com>
2023-04-17 22:17:01 +0530

    ninth

--- script.py
+++ script.py
@@ -13,7 +13,7 @@ import yaml
 
 myclient2 = boto3.client('ec2','us-east-2')
 
-with open('ec2.yaml','r') as e:
+with open('ec2.yml','r') as e:
     mydata2 = yaml.load(e,Loader=yaml.FullLoader)
 
 myclient2.run_instances(**mydata2)
