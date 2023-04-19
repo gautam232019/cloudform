4e3df42d6230964f6e0f0d76b6887481d3e194f0
Gautam Sachdeva <56038800+gautam232019@users.noreply.github.com>
2023-04-19 14:26:31 +0530

    2_accounts_res_creation

--- script2.py
+++ script2.py
@@ -3,22 +3,16 @@ import yaml
 import os
 
 myclient2 = boto3.client('ec2','us-east-2')
-def Convert(lst):
-    res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
-    return res_dct 
 
 with open('ec2.yml','r') as e:
-    mydata2 = yaml.load(e,Loader=yaml.FullLoader)
-    mydictionary = Convert(mydata2)
+    mydata2 = yaml.safe_load(e,Loader=yaml.FullLoader)
     if(os.environ['InstanceType']):
-        mydictionary["InstanceType"] = os.environ['InstanceType']
+        mydata2["InstanceType"] = os.environ['InstanceType']
     if(os.environ['InstanceName']):
-        mydictionary["TagSpecifications"]["Tags"]["Value"] = os.environ['InstanceName']
-
-list = [(k, v) for k, v in mydictionary.items()]
+        mydata2["TagSpecifications"]["Tags"]["Value"] = os.environ['InstanceName']
 
 with open("ec2.yml", "w") as f:
-    yaml.dump(list, f)
+    yaml.dump(mydata2, f)
 
 with open('ec2.yml','r') as e:
     mydata2 = yaml.load(e,Loader=yaml.FullLoader)
