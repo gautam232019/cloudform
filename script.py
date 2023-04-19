470bf60ac78c9e4da3a4f8eaaf6776e2ddf68040
Gautam Sachdeva <56038800+gautam232019@users.noreply.github.com>
2023-04-19 12:02:28 +0530

    2_accounts_res_creation

--- script.py
+++ script.py
@@ -1,12 +1,14 @@
 import boto3
 import yaml
+import os
 
 myclient = boto3.client('s3')
 
 with open('cf.yml','r') as f:
     mydata = yaml.load(f,Loader=yaml.FullLoader)
 
-bucket_name = mydata['BucketName']
+# bucket_name = mydata['BucketName']
+bucket_name = os.environ['BucketName']
 # location = config.get('Location','us-east-2')
 myclient.create_bucket(Bucket=bucket_name,CreateBucketConfiguration={'LocationConstraint':'us-east-2'},)
 
