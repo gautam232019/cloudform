b9043553442f49679e68cbc261e9fbb1d6b36231
Gautam Sachdeva <56038800+gautam232019@users.noreply.github.com>
2023-04-19 14:34:12 +0530

    2_accounts_res_creation

--- script2.py
+++ script2.py
@@ -9,7 +9,8 @@ with open('ec2.yml','r') as e:
     if(os.environ['InstanceType']):
         mydata2["InstanceType"] = os.environ['InstanceType']
     if(os.environ['InstanceName']):
-        mydata2["TagSpecifications"][1][1] = os.environ['InstanceName']
+        print(mydata2["TagSpecifications"])
+        mydata2["TagSpecifications"] = os.environ['InstanceName']
 
 with open("ec2.yml", "w") as f:
     yaml.dump(mydata2, f)
