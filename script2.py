b6e32e06d25ffe05d6fa78853164b3b8b02337c5
Gautam Sachdeva <56038800+gautam232019@users.noreply.github.com>
2023-04-20 17:32:45 +0530

    2_accounts_res_creation

--- script2.py
+++ script2.py
@@ -3,6 +3,11 @@ import yaml
 import os
 
 myclient2 = boto3.client('ec2','us-east-2')
+iam = boto3.client('iam')
+
+role_name = 'accessS3UsingEc2'
+policy_name = "accessS3"
+
 
 with open('ec2.yml','r') as e:
     mydata2 = yaml.safe_load(e)
@@ -18,4 +23,39 @@ with open("ec2.yml", "w") as f:
 with open('ec2.yml','r') as e:
     mydata2 = yaml.load(e,Loader=yaml.FullLoader)
 
-myclient2.run_instances(**mydata2)
+response = iam.create_role(
+    RoleName=role_name,
+    AssumeRolePolicyDocument='''{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "ec2.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }'''
+)
+bucket_name = os.environ['BucketName']
+policy = {
+    'Version': '2012-10-17',
+    'Statement': [
+        {
+            'Effect': 'Allow',
+            'Action': [
+                's3:GetObject',
+                's3:PutObject',
+                's3:PutObjectAcl'
+            ],
+            'Resource': f'arn:aws:s3:::{bucket_name}/*'
+        }
+    ]
+}
+
+iam.put_role_policy(RoleName=role_name,PolicyName=policy_name,PolicyDocument=json.dumps(policy))
+iam.create_instance_profile(InstanceProfileName=role_name)
+
+
+myclient2.run_instances(**mydata2+f",IamInstanceProfile={{'Name': {role_name}}}")
